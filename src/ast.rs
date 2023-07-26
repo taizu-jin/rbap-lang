ddef9422b793cc809d67cafbbbce775412d5c7b6
taizu-jin <mantas.scc@gmail.com>
2023-07-26 13:30:59 +0300

    feat(ast): add DataDeclaration statement

--- src/ast.rs
+++ src/ast.rs
@@ -3,12 +3,25 @@ use crate::lexer::Token;
 #[derive(Debug)]
 pub enum Statement {
     Expression(Expression),
+    DataDeclaration(DataDeclaration),
 }
 
 #[derive(Debug)]
 pub enum Expression {
     IntLiteral { value: i64, token: Token },
     StringLiteral { value: String, token: Token },
+}
+
+#[derive(Debug)]
+pub struct DataDeclaration {
+    pub ident: String,
+    pub ty: DataType,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum DataType {
+    String,
+    Int,
 }
 
 #[derive(Debug)]
