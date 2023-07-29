d6e39e6ec294937d5486cf95fffc0d584131025f
taizu-jin <mantas.scc@gmail.com>
2023-07-29 13:27:55 +0300

    feat(parser): parse Ident expression

--- src/parser.rs
+++ src/parser.rs
@@ -112,7 +112,8 @@ impl Parser {
                 Self::parse_integer_literal(literal.as_str(), &mut self.carriage.errors)
             }
             Token::StringLiteral(literal) => Some(Self::parse_string_literal(literal.as_str())),
-            _ => unimplemented!("{:?}", self.carriage.cur_token),
+            Token::Ident(ident) => Some(Expression::Ident(ident.to_owned())),
+            _ => unimplemented!("can't parse expression '{}(:?)'", self.carriage.cur_token),
         }
     }
 
