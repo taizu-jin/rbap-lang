54bab0fc31c84745712bbab063e821217a34b221
taizu-jin <mantas.scc@gmail.com>
2023-08-05 07:38:47 +0300

    refactor(parser): remove lifetime from parser Error

--- src/parser/error.rs
+++ src/parser/error.rs
@@ -2,18 +2,21 @@ use std::num::ParseIntError;
 
 use thiserror::Error;
 
-use crate::lexer::Token;
+use crate::lexer::{TokenKind, TokenKinds};
 
-pub type Result<'a, T> = std::result::Result<T, Error<'a>>;
+pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, Error)]
-pub enum Error<'a> {
+pub enum Error {
     #[error(transparent)]
     ParseInt(#[from] ParseIntError),
-    #[error("unexpected token: `{0}`")]
-    ExpectToken(Token<'a>),
-    #[error("expected an expression")]
-    ParseDataAssingment,
-    #[error("Unrecognized string template token: {0}")]
-    ParseStringTemplate(Token<'a>),
+    #[error("expected tokens: {expected}. got={got:?}")]
+    ExpectToken {
+        got: Option<TokenKind>,
+        expected: TokenKinds,
+    },
+    #[error("unrecognized string template token: \n\tkind - {kind}\n\tliteral - {literal}")]
+    ParseStringTemplate { kind: TokenKind, literal: String },
+    #[error("expected a token, but reached EOF")]
+    Eof,
 }
