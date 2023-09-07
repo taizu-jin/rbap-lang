cbb233c14be8753b3ee22b1a9e364df36aab7631
taizu-jin <mantas.scc@gmail.com>
2023-09-07 09:50:41 +0300

    ref(parser): move block statement code to its own module

---
+++ src/ast/statement/block.rs
@@ -0,0 +1,38 @@
+use std::fmt::Display;
+
+use crate::{error::Result, lexer::TokenKind, parser::Carriage};
+
+use super::Statement;
+
+#[derive(Debug, PartialEq)]
+pub struct Block {
+    pub statements: Vec<Statement>,
+}
+
+impl Block {
+    pub fn parse(carriage: &mut Carriage) -> Result<Self> {
+        let mut statements = Vec::new();
+
+        while !(carriage.is_peek_token(TokenKind::EndIf)
+            || carriage.is_peek_token(TokenKind::Else)
+            || carriage.is_peek_token(TokenKind::EndMethod))
+        {
+            let statement = Statement::parse(carriage)?;
+            statements.push(statement);
+        }
+
+        let block = Block { statements };
+
+        Ok(block)
+    }
+}
+
+impl Display for Block {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for s in &self.statements {
+            write!(f, "{}", s)?;
+        }
+
+        Ok(())
+    }
+}
