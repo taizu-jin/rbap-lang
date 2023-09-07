d7c1df5803ab313741d321f25b10b0d31f8fe2cf
taizu-jin <mantas.scc@gmail.com>
2023-09-07 10:43:55 +0300

    ref(parser): move write statement to its own module

--- src/ast/statement.rs
+++ src/ast/statement.rs
@@ -3,11 +3,12 @@ mod block;
 mod declaration;
 mod function;
 mod if_statement;
+mod write;
 
 use crate::{
     error::Result,
     lexer::TokenKind,
-    parser::{context::PeekToken, parse, Carriage, Context},
+    parser::{parse, Carriage, Context},
 };
 
 use std::fmt::Display;
@@ -18,15 +19,16 @@ pub use self::{
     declaration::{Data, DataType, Declaration},
     function::Function,
     if_statement::IfStatement,
+    write::Write,
 };
 
-use super::{primitive, Expression};
+use super::Expression;
 
 #[derive(Debug, PartialEq)]
 pub enum Statement {
     Expression(Expression),
-    Write(Vec<Expression>),
     Declaration(Declaration),
+    Write(Write),
     Assignment(Assignment),
     Block(Block),
     If(IfStatement),
@@ -42,7 +44,7 @@ impl Statement {
             TokenKind::Ident if context.peek_token.kind == TokenKind::Assign => {
                 parse(carriage, &context, Assignment::parse)?.into()
             }
-            TokenKind::Write => parse(carriage, &context, Self::parse_write_statement)?,
+            TokenKind::Write => parse(carriage, &context, Write::parse)?.into(),
             TokenKind::If => parse(carriage, &context, IfStatement::parse)?.into(),
             TokenKind::Method => parse(carriage, &context, Function::parse)?.into(),
             _ => Statement::Expression(parse(carriage, &context, Expression::parse)?),
@@ -51,55 +53,6 @@ impl Statement {
         carriage.expect_tokens(&[TokenKind::Period])?;
 
         Ok(statement)
-
-    fn parse_write_statement(
-        carriage: &mut Carriage,
-        PeekToken(peek_token): PeekToken,
-    ) -> Result<Self> {
-        let mut expressions = Vec::new();
-
-        match peek_token.kind == TokenKind::Colon {
-            true => {
-                carriage.next_token()?;
-
-                loop {
-                    if carriage.is_peek_token(TokenKind::Slash) {
-                        expressions.push(primitive::String::from("\n").into());
-                        carriage.next_token()?;
-                    }
-
-                    expressions.push(Self::expect_and_parse_expression(carriage)?);
-
-                    if carriage.is_peek_token(TokenKind::Comma) {
-                        carriage.next_token()?;
-                    } else {
-                        break;
-                    }
-                }
-            }
-            false => {
-                if carriage.is_peek_token(TokenKind::Slash) {
-                    expressions.push(primitive::String::from("\n").into());
-                    carriage.next_token()?;
-                }
-                expressions.push(Self::expect_and_parse_expression(carriage)?)
-            }
-        }
-
-        Ok(Statement::Write(expressions))
-    }
-
-    fn expect_and_parse_expression(carriage: &mut Carriage) -> Result<Expression> {
-        let token = carriage.expect_tokens(&[
-            TokenKind::Ident,
-            TokenKind::IntLiteral,
-            TokenKind::StringLiteral,
-            TokenKind::VSlash,
-        ])?;
-
-        let context = Context::new(token, carriage.peek_token()?.clone());
-
-        parse(carriage, &context, Expression::parse)
     }
 }
 
@@ -109,47 +62,10 @@ impl Display for Statement {
             Statement::Expression(e) => write!(f, "{}.", e),
             Statement::Function(func) => write!(f, "{}", func),
             Statement::Block(b) => write!(f, "{}", b),
-            Statement::Write(strings) => {
-                write!(f, "WRITE:")?;
-                for s in strings {
-                    match s {
-                        Expression::StringLiteral(s) if s.as_ref() == "\n" => write!(f, " /")?,
-                        e => write!(f, " {}", e)?,
-                    }
-                }
-                write!(f, ".")
-            }
             Statement::If(s) => write!(f, "{}", s),
             Statement::Assignment(d) => write!(f, "{}", d),
             Statement::Declaration(d) => write!(f, "{}", d),
             Statement::Write(w) => write!(f, "{}", w),
         }
-    }
-}
-
-#[derive(Debug, PartialEq, Copy, Clone)]
-pub enum DataType {
-    String,
-    Int,
-}
-
-impl Display for DataType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            DataType::String => write!(f, "string"),
-            DataType::Int => write!(f, "i"),
-        }
-    }
-}
-
-#[derive(Debug, PartialEq)]
-pub struct DataDeclaration {
-    pub ident: String,
-    pub ty: DataType,
-}
-
-impl Display for DataDeclaration {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} TYPE {}", self.ident, self.ty)
     }
 }
