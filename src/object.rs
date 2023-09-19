8f86e1baba44b57782e32f1a864340984940a885
taizu-jin <mantas.scc@gmail.com>
2023-09-19 14:33:30 +0300

    feat(compiler): make compiled objects cloneable and implement Into<DataType>

--- src/object.rs
+++ src/object.rs
@@ -2,9 +2,9 @@
 
 use std::fmt::Display;
 
-use crate::code::Instructions;
+use crate::{ast::DataType, code::Instructions};
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Object {
     String(String),
     Int(i64),
@@ -23,7 +23,7 @@ impl Display for Object {
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct CompiledFunction {
     pub instructions: Instructions,
     pub num_locals: usize,
@@ -44,5 +44,16 @@ impl Display for CompiledFunction {
 impl From<CompiledFunction> for Object {
     fn from(value: CompiledFunction) -> Self {
         Object::Function(value)
+    }
+}
+
+impl From<Object> for DataType {
+    fn from(value: Object) -> Self {
+        match value {
+            Object::String(_) => DataType::String,
+            Object::Int(_) => DataType::Int,
+            Object::Bool(_) => DataType::Bool,
+            Object::Function(_) => todo!(),
+        }
     }
 }
