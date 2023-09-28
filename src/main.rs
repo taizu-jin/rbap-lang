9888d82379a30365731581640b697fc3c084b43a
taizu-jin <mantas.scc@gmail.com>
2023-09-28 09:45:20 +0300

    feat(cli): implement compile subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-use std::io;
+use std::path::PathBuf;
 
-use rbap_lang::{cli::Cli, start, Parser};
+use rbap_lang::{cli::Cli, start, Compiler, Parser, Result};
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
     match cli.command {
         Some(commands) => match commands {
-            rbap_lang::cli::Commands::Compile(_) => todo!(),
+            rbap_lang::cli::Commands::Compile(c) => {
+                let src = PathBuf::from(&c.src);
+                let dest = c.dest.map_or_else(
+                    || {
+                        let mut dest = src.clone();
+                        dest.set_extension("rbap");
+                        dest
+                    },
+                    |dest| PathBuf::from(&dest),
+                );
+
+                let compiler = Compiler::compile(src)?;
+                compiler.into_file(dest)?;
+            }
             rbap_lang::cli::Commands::Run(_) => todo!(),
         },
         None => start()?,
